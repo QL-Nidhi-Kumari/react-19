36f4841dc781e244c089519360480f259c5e10a1
NIDHI KUMARI <nidhi.kumari@quokkalabs.com>
2024-09-12 22:45:10 +0530

    useform status feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import "./App.css";
-import LoginOld from "./components/login";
-import LoginAction from "./components/login-action";
 import LoginReact19 from "./components/login-react-19";
 
 function App() {
   return (
     <div>
-      <LoginOld />
-      <hr />
-      <LoginAction />
-      <hr />
       <LoginReact19 />
     </div>
   );
--- src/components/login-react-19.jsx
+++ src/components/login-react-19.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useActionState } from "react";
 import { loginUser } from "../api/user";
-import { useActionState } from 'react';
+import { useFormStatus } from "react-dom";
+
+const SubmitButton = () => {
+  const { pending } = useFormStatus();
+
+  return (
+    <button type="submit" disabled={pending}>
+      {pending ? "Logging in..." : "Login"}
+    </button>
+  );
+};
 
 const LoginReact19 = () => {
-  const [user, submitAction, isPending] = useActionState(login, {
+  const [
+    user,
+    submitAction,
+    // isPending
+  ] = useActionState(login, {
     error: null,
     data: null,
   });
@@ -12,7 +26,7 @@ const LoginReact19 = () => {
     const username = formData.get("username");
     const password = formData.get("password");
     try {
-      const response: any = await loginUser(username, password);
+      const response = await loginUser(username, password);
       return { error: null, data: response.data };
     } catch (error) {
       return { ...previousState, error: error.error };
@@ -29,9 +43,8 @@ const LoginReact19 = () => {
         <label>Password:</label>
         <input name="password" type="password" required />
       </div>
-      <button type="submit" disabled={isPending}>
-        {isPending ? "Logging in..." : "Login"}
-      </button>
+
+      <SubmitButton />
 
       {user.data && (
         <p style={{ color: "green" }}>Logged in: {user.data.email}</p>
